e9f8ec85e29d3b97f7c2293966e60835a4e7ac9f
Radojica Popovc <108525632+radojicapopovic@users.noreply.github.com>
2022-11-07 12:16:34 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import React from "react";
 import "./App.css";
+import InputFeild from "./components/InputFeild";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  )  
-}
+const App: React.FC = () => {
+  return <div className="App">
+    <span className="heading">Taskify</span>
+    <InputFeild/>
+  </div>;
+};
 
 export default App;
